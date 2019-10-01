456ef49509747135aafb294aeace7c3b7bc02070
siko408 <gamerbloxhd@gmail.com>
2019-09-30 23:49:38 -0700

    Game loop

--- superheroes.py
+++ superheroes.py
@@ -286,13 +286,27 @@ class Arena:
         self.team_two.stats()
 
 
-if(__name__ == "__main__"):
-    while(True):
-        if(input("Would you like to exit?[Y/N]") == "Y"):
-            break
+if __name__ == "__main__":
+    game_is_running = True
+
+    # Instantiate Game Arena
+    arena = Arena()
+
+    #Build Teams
+    arena.build_team_one()
+    arena.build_team_two()
+
+    while game_is_running:
+
+        arena.team_battle()
+        arena.show_stats()
+        play_again = input("Play Again? Y or N: ")
+
+        #Check for Player Input
+        if play_again.lower() == "n":
+            game_is_running = False
+
         else:
-            arena = Arena()
-            arena.build_team_one()
-            arena.build_team_two()
-            arena.team_battle()
-            arena.show_stats()
+            #Revive heroes to play again
+            arena.team_one.revive_heroes()
+            arena.team_two.revive_heroes()
